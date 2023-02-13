3722b4ab5e3fbd0d151be1fdd89662d0184f7857
Maddie Wong <maddie12@outlook.com>
2023-02-13 11:30:57 -0500

    Add pages and formatting

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route('/experience.html')
+def experience():
+    return render_template("experience.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
