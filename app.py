8f397fa75b1d9ed2a8232ab4fddd6a0891d02c08
Maddie Wong <maddie12@outlook.com>
2023-02-14 16:03:39 -0500

    Add content to projects page and index page

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home_page():
 def experience():
     return render_template("experience.html")
 
+@app.route('/projects.html')
+def projects():
+    return render_template("projects.html")
+
 @app.route('/contact.html')
 def contact():
     return render_template("contact.html")
